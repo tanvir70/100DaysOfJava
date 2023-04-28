38e331b4257c1546b83166c8d1bed34d57583da7
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2023-04-28 18:53:09 +0600

    Day 54
    Implement Text Editor Undo Redo
    We aim to implement rudimentary undo & redo.
    
    You will be provided a set of actions to perform. Once all actions are performed you will return the current state the system should be in after all actions in actions are performed.
    
    We will be operating on characters and the "state" of the system will be a string that we are building.
    
    These are the actions possible:
    INSERT: Inserts a single character to the end of the string
    DELETE: Removes the last character in the string
    UNDO: Reverses the most recent action
    REDO: Redoes the most recent action undone
    
    Your inputs will only be single characters. There are only 4 input actions as enumerated above.
    
    Example 1:
    Input:
    INSERT 'a'
    INSERT 'b'
    
    Output: "ab"
    
    Example 2:
    Input:
    INSERT 'a'
    INSERT 'b'
    UNDO
    
    Output: "a"
    
    Example 3:
    Input:
    INSERT 'a'
    INSERT 'b'
    UNDO
    REDO
    
    Output: "ab"
    
    Example 4:
    Input:
    INSERT 'a'
    INSERT 'b'
    UNDO
    REDO
    REDO # Does nothing
    
    Output: "ab"

---
+++ TextEditor.java
@@ -0,0 +1,127 @@
+package swe.StackQueue;
+
+import java.util.Stack;
+
+class Action {
+    Character character;
+    ActionType type;
+
+    public Action(ActionType type) {
+        this.type = type;
+    }
+
+    public Action(ActionType type, Character character) {
+        this.character = character;
+        this.type = type;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public ActionType getType() {
+        return type;
+    }
+}
+
+enum ActionType {
+    INSERT,
+    DELETE,
+    UNDO,
+    REDO
+}
+
+public class TextEditor {
+    private String currentState;
+    private Stack<Action> undo;
+    private Stack<Action> redo;
+
+    public TextEditor() {
+        currentState = "";
+        undo = new Stack<>();
+        redo = new Stack<>();
+
+    }
+
+    public String performEditorAction(Action type) {
+        switch (type.getType()) {
+            case INSERT -> {
+                insert(type.getCharacter());
+                break;
+            }
+            case DELETE -> {
+                delete();
+                break;
+            }
+            case REDO -> {
+                redo();
+                break;
+            }
+            case UNDO -> {
+                undo();
+                break;
+            }
+        }
+        return currentState;
+    }
+
+    private void insert(char c) {
+        undo.push(new Action(ActionType.DELETE));
+        currentState += c;
+        redo.clear();
+    }
+
+    private void delete() {
+        if (!currentState.isEmpty()) {
+            undo.push(new Action(ActionType.INSERT, currentState.charAt(currentState.length() - 1)));
+            currentState = currentState.substring(0, currentState.length() - 1);
+            redo.clear();
+        }
+    }
+
+    private void undo() {
+        if (!undo.isEmpty()) {
+            Action lastAction = undo.pop();
+            redo.push(lastAction);
+            switch (lastAction.getType()) {
+                case INSERT -> {
+                    delete();
+                    break;
+                }
+                case DELETE -> {
+                    insert(lastAction.getCharacter());
+                    break;
+                }
+            }
+        }
+    }
+
+    private void redo() {
+        if (!redo.isEmpty()) {
+            Action lastAction = redo.pop();
+            undo.push(lastAction);
+            switch (lastAction.getType()) {
+                case INSERT -> {
+                    insert(lastAction.getCharacter());
+                    break;
+                }
+                case DELETE -> {
+                    delete();
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TextEditor textEditor = new TextEditor();
+        textEditor.performEditorAction(new Action(ActionType.INSERT,'H'));
+        textEditor.performEditorAction(new Action(ActionType.INSERT,'e'));
+        textEditor.performEditorAction(new Action(ActionType.INSERT,'l'));
+        textEditor.performEditorAction(new Action(ActionType.INSERT,'l'));
+        textEditor.performEditorAction(new Action(ActionType.INSERT,'o'));
+        textEditor.performEditorAction(new Action(ActionType.INSERT,'H'));
+        textEditor.performEditorAction(new Action(ActionType.DELETE));
+        System.out.println(textEditor.currentState);
+    }
+}
